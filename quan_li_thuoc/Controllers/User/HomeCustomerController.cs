f9c79020067ec6da80d6a50fe4d93726053d0c86
NguyenXuanDung <nguyenxuand977@gmail.com>
2025-11-29 19:12:59 +0700

    xuandung commit lan dau

---
+++ quan_li_thuoc/Controllers/User/HomeCustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quan_li_thuoc.Controllers.User
+{
+    public class HomeCustomerController : Controller
+    {
+        // GET: HomeCustomer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
